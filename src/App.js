588f01d7843b8d329d88666f8935e417141b5f7d
Eric K <kreitere@gmail.com>
2019-11-04 11:26:54 -0500

    Feature: Collection component, collection preivew component, shop page linked into react router and styling finished for these items

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 //As soon as Switch see's a match it will not render any other component after that.
 
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
