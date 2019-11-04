333e325796405d3a8602381ee1638c50ca919bb5
Eric K <kreitere@gmail.com>
2019-11-03 20:14:11 -0500

    Update: Created Folder Structure and made components more modular and DRY

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+//destructuring our title off our props and setting it to the value of title
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
